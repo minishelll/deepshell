ffaf23261c8cdc6c9303046e43b1177e1cbb5a9f
김태란 <taerankim@gimtaelans-MacBook-Air.local>
2024-04-04 23:04:21 +0900

    copy tokenizer file from 54a340a

---
+++ src/manda/parser/tokenizer.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taerakim <taerakim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 10:10:43 by sehwanii          #+#    #+#             */
+/*   Updated: 2024/03/30 22:37:41 by taerakim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+t_list	*parse_quote(char *command);
+t_list	*parse_space(t_list *quote_parsed_list);
+void	add_end_token(t_list *parsed_list);
+
+t_list	*tokenizer(char *command)
+{
+	t_list	*quote_parsed_list;
+	t_list	*op_parsed_list;
+
+	quote_parsed_list = parse_quote(command);
+	op_parsed_list = parse_op(quote_parsed_list);
+	add_end_token(op_parsed_list);
+	free(quote_parsed_list);
+	return (op_parsed_list);
+}
+
+void	free_token(t_token *token)
+{
+	free(token->word);
+	free(token);
+}
+
+void	add_end_token(t_list *parsed_list)
+{
+	t_token	*end_token;
+
+	end_token = (t_token *)ft_malloc(sizeof(t_token));
+	end_token -> word = ft_strdup("$");
+	end_token -> type = dollar_sign;
+	ft_lstadd_back(&parsed_list, ft_lstnew(end_token));
+	return ;
+}
