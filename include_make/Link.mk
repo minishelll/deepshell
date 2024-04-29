21cd03104dc30a7536da210b6206ccbc3d1deac9
sehwanii <sehwan1023@gmail.com>
2024-04-29 23:53:35 +0900

    Revert "Dev/wildcard"

--- include_make/Link.mk
+++
@@ -1,6 +0,0 @@
-link_files:: unlink_files
-	@mkdir -p $(dst_dir);
-	$(foreach file,$(files), ln -sf $(src_dir)/$(file) $(dst_dir)/$(files);)
-
-unlink_files::
-	$(foreach file,$(files), $(RM) $(dst_dir)/$(files);)
