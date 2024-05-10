f9c54986f7b707923568c48d9fd8d3940bdf81f1
Taeran <135775997+taerakim@users.noreply.github.com>
2024-05-10 18:24:40 +0900

    Merge pull request Dev/wildcard expand (#45)

---
+++ src/manda/expand/wildcard.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcard.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sehwjang <sehwjang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 21:28:44 by sehwjang          #+#    #+#             */
+/*   Updated: 2024/05/08 17:20:56 by sehwjang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WILDCARD_H
+# define WILDCARD_H
+
+# include "libft.h"
+# include <stdbool.h>
+
+/* ******************************** LITERAL ********************************* */
+
+/* ********************************* STRUCT ********************************* */
+
+/* ******************************** FUNCTION ******************************** */
+bool	check_word(char *str1, char *str2);
+// bool	match_and_collect(char *word, t_list *dir_lst, t_list **matched);
+// bool	match_wildcard(t_list *token_prev, t_list **token_cur, t_list *dir_lst);
+// void	process_wildcard_tokens(t_list *token, t_list *dir_lst);
+/* wildcard_utils */
+void	attach_head_node(t_list **token);
+t_list	*get_dir_lst(void);
+void	wildcard_add_back(t_list **token_list, char *str);
+bool	**make_word_table(char *str1, char *str2);
+void	match_word(char *word, t_list *dir_lst, t_list **matched, bool flag);
+void	expand_wildcard(t_list **head, t_list **curr);
+void ft_lstsort(t_list **lst, int (*sort)(const char *, const char *, size_t n));
+void ft_lstswap(t_list *a, t_list *b);
+void	dummy_add_back(t_list **token_list);
+#endif
